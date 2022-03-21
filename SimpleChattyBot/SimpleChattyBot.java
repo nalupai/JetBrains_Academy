145c99ad865393242c5cfcd62bd29dd8b0c0715d
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-03-21 16:37:36 +0100

    Work on project. Stage 1/5
    Simple Chatty Bot welcomes you

---
+++ SimpleChattyBot/SimpleChattyBot.java
@@ -0,0 +1,6 @@
+public class SimpleChattyBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Simple Chatty Bot.");
+        System.out.println("I was created in 2022.");
+    }
+}
