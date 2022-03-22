8ea522acf7808ded152eadb0fd23becffd95ddf8
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-03-22 12:53:30 +0100

    Work on project. Stage 2/5
    Print your name

--- SimpleChattyBot/SimpleChattyBot.java
+++ SimpleChattyBot/SimpleChattyBot.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class SimpleChattyBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! My name is Simple Chatty Bot.");
         System.out.println("I was created in 2022.");
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.next();
+
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
