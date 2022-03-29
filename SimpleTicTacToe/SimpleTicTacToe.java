b5839f9f7c6e1e434f4082d94ab607e48f94aa28
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-03-29 13:57:44 +0200

    Work on project. Stage 2/5
    The user is the gamemaster

--- SimpleTicTacToe/SimpleTicTacToe.java
+++ SimpleTicTacToe/SimpleTicTacToe.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class SimpleTicTacToe {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X O X");
+        Scanner scanner = new Scanner(System.in);
+
+        String game = scanner.next();
+        
+        System.out.println("---------");
+        System.out.println("| " + game.charAt(0) + " " + game.charAt(1) + " " + game.charAt(2) + " |");
+        System.out.println("| " + game.charAt(3) + " " + game.charAt(4) + " " + game.charAt(5) + " |");
+        System.out.println("| " + game.charAt(6) + " " + game.charAt(7) + " " + game.charAt(8) + " |");
+        System.out.println("---------");
     }
 }
