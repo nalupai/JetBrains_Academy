46dbe607594caaa016bf9bdd509994aa2acc8871
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-04-04 15:32:07 +0200

    Work on project. Stage 1/6
    Making coffee

---
+++ CoffeeMachine/CoffeeMachine.java
@@ -0,0 +1,13 @@
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        String stepsNecessaryToMakeCoffee = """
+                                        Starting to make a coffee
+                                        Grinding coffee beans
+                                        Boiling water
+                                        Mixing boiled water with crushed coffee beans
+                                        Pouring coffee into the cup
+                                        Pouring some milk into the cup
+                                        Coffee is ready!""";
+        System.out.println(stepsNecessaryToMakeCoffee);
+    }
+}
