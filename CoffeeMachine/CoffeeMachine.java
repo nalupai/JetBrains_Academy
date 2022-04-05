99dfb092b5e58d5ed9cff9e7b469d3bde1039bbc
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-04-05 14:23:25 +0200

    Work on project. Stage 2/6
    Ingredient calculator

--- CoffeeMachine/CoffeeMachine.java
+++ CoffeeMachine/CoffeeMachine.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String stepsNecessaryToMakeCoffee = """
                                         Starting to make a coffee
                                         Grinding coffee beans
@@ -8,6 +12,20 @@ public class CoffeeMachine {
                                         Pouring coffee into the cup
                                         Pouring some milk into the cup
                                         Coffee is ready!""";
+                                        
         System.out.println(stepsNecessaryToMakeCoffee);
+
+        System.out.println("Write how many cups of coffee you will need:");
+        
+        int howManyCups = scanner.nextInt();
+        
+        int water = 200;
+        int milk = 50;
+        int beans = 15;
+        
+        System.out.println("For " + howManyCups + " cups of coffee you will need:");
+        System.out.println(howManyCups * water + " ml of water");
+        System.out.println(howManyCups * milk + " ml of milk");
+        System.out.println(howManyCups * beans + " g of coffee beans");
     }
 }
