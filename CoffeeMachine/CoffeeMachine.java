8186de5f6efb961a9cae82ca1896eee5c53ac89e
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-04-06 15:19:10 +0200

    Work on project. Stage 3/6
    Estimate the number of servings

--- CoffeeMachine/CoffeeMachine.java
+++ CoffeeMachine/CoffeeMachine.java
@@ -4,28 +4,40 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String stepsNecessaryToMakeCoffee = """
-                                        Starting to make a coffee
-                                        Grinding coffee beans
-                                        Boiling water
-                                        Mixing boiled water with crushed coffee beans
-                                        Pouring coffee into the cup
-                                        Pouring some milk into the cup
-                                        Coffee is ready!""";
-                                        
-        System.out.println(stepsNecessaryToMakeCoffee);
-
+        int requiredWater = 200;
+        int requiredMilk = 50;
+        int requiredBeans = 15;
+        
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int beans = scanner.nextInt();
         System.out.println("Write how many cups of coffee you will need:");
+        int order = scanner.nextInt();
         
-        int howManyCups = scanner.nextInt();
+        int cupsOfWater = water / requiredWater;
+        int cupsOfMilk = milk / requiredMilk;
+        int cupsOfBeans = beans / requiredBeans;
         
-        int water = 200;
-        int milk = 50;
-        int beans = 15;
+        int cups = 0;
         
-        System.out.println("For " + howManyCups + " cups of coffee you will need:");
-        System.out.println(howManyCups * water + " ml of water");
-        System.out.println(howManyCups * milk + " ml of milk");
-        System.out.println(howManyCups * beans + " g of coffee beans");
+        if (cupsOfWater <= cupsOfMilk && cupsOfWater <= cupsOfBeans) {
+            cups = cupsOfWater;    
+        } else if (cupsOfMilk <= cupsOfWater && cupsOfMilk <= cupsOfBeans) {
+            cups = cupsOfMilk;    
+        } else if (cupsOfBeans <= cupsOfWater && cupsOfBeans <= cupsOfMilk) {
+            cups = cupsOfBeans;    
+        }
+        
+        if (cups < order) {
+            System.out.println("No, I can make only " + cups + " cup(s) of coffee");
+        } else if (cups == order) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cups > order) {
+            int delta = cups - order;
+            System.out.println("Yes, I can make that amount of coffee (and even " + delta + " more than that)");
+        }
     }
 }
