37e74b3bca7ce9ecab587e7c53e10c80479ddd0f
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-04-19 11:39:41 +0200

    Work on project. Stage 2/5
    Sold!

--- CinemaRoomManager/CinemaRoomManager.java
+++ CinemaRoomManager/CinemaRoomManager.java
@@ -1,16 +1,41 @@
+import java.util.Scanner;
+
 public class CinemaRoomManager {
     
     public static void main(String[] args) {
 
-        int cinemaRoomHeight = 7, cinemaRoomWidth = 8;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int cinemaRoomHeight = scanner.nextInt();
+
+        System.out.println("Enter the number of seats in each row:");
+        int cinemaRoomWidth = scanner.nextInt();
+
+        //int cinemaRoomHeight = 7, cinemaRoomWidth = 8;
 
         char[][] cinemaRoom = new char[cinemaRoomHeight][cinemaRoomWidth];
 
-        prepareRoom(cinemaRoom, cinemaRoomHeight, cinemaRoomWidth);
+        System.out.println("Total income:\n$" + totalIncome(cinemaRoom, cinemaRoomHeight, cinemaRoomWidth));
 
-        System.out.println("Cinema:");
+        //prepareRoom(cinemaRoom, cinemaRoomHeight, cinemaRoomWidth);
 
-        showRoom(cinemaRoom, cinemaRoomHeight, cinemaRoomWidth);
+        //System.out.println("Cinema:");
+
+        //showRoom(cinemaRoom, cinemaRoomHeight, cinemaRoomWidth);
+    }
+
+    public static int totalIncome(char[][] room, int height, int width) {
+
+        if (height * width < 60) {
+            return height * width * 10;
+        } else {
+            if (height % 2 != 0) {
+                return height / 2 * 10 * width + (height - height / 2) * 8 * width;
+            } else {
+                return height / 2 * 10 * width + height / 2 * 8 * width;
+            }
+        }
     }
 
     public static void showRoom(char[][] room, int height, int width) {
