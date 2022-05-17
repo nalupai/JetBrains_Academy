5fffa2c83b86c0ce0c4dfc1141f904cd72c7d01b
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-05-17 13:25:02 +0200

    Work on project. Stage 2/7
    Grader

--- BullsAndCows/BullsAndCows.java
+++ BullsAndCows/BullsAndCows.java
@@ -1,14 +1,41 @@
+import java.util.Scanner;
+
 public class BullsAndCows {
     public static void main(String[] args) {
-        System.out.println("The secret code is prepared: ****.");
-        System.out.println();
-        System.out.println("Turn 1. Answer:");
-        System.out.println(1234);
-        System.out.println("Grade: None.");
-        System.out.println();
-        System.out.println("Turn 2. Answer:");
-        System.out.println(9876);
-        System.out.println("Grade: 4 bulls.");
-        System.out.println("Congrats! The secret code is 9876.");
+        Scanner scanner = new Scanner(System.in);
+
+        String code = String.valueOf(9305);
+        int bulls = 0;
+        int cows = 0;
+        String userInput = scanner.nextLine();
+        String[] answer = userInput.split("");
+
+        for (int i = 0; i < answer.length; i++) {
+            int index = code.indexOf(answer[i]);
+            if (index == i) {
+                bulls++;
+            } else if (index >= 0) {
+                cows++;
+            }
+        }
+
+        System.out.println(grade(bulls, cows, code));
+        scanner.close();
+    }
+
+    private static String grade(int bulls, int cows, String code) {
+        String result;
+
+        if (bulls > 0 && cows > 0) {
+            result = String.format("%d bull(s) and %d cow(s)", bulls, cows);
+        } else if (bulls > 0) {
+            result = String.format("%d bull(s)", bulls);
+        } else if (cows > 0) {
+            result = String.format("%d cow(s)", cows);
+        } else {
+            result = "None";
+        }
+
+        return String.format("Grade: %s. The secret code is %s.", result, code);
     }
 }
