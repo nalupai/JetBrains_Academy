418929fc512d9e815a6e11a12fce720f37b8a237
springwithjava <102017422+springwithjava@users.noreply.github.com>
2022-05-19 18:39:08 +0200

    Work on project. Stage 4/7
    Game time!

--- BullsAndCows/BullsAndCows.java
+++ BullsAndCows/BullsAndCows.java
@@ -4,29 +4,76 @@ import java.util.Scanner;
 public class BullsAndCows {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
 
+        System.out.println("Please, enter the secret code's length:");
         int size = scanner.nextInt();
         int maxSize = 10;
-        int newDigit;
-        String code = "";
 
-        if (size <= maxSize) {
-            newDigit = random.nextInt(9) + 1;
-            code = code + newDigit;
-            while (code.length() < size) {
-                newDigit = random.nextInt(9);
-                //code.indexOf(String.valueOf(newDigit)) != -1
-                if (code.contains(String.valueOf(newDigit))) {
-                    continue;
-                }
-                code = code + newDigit;
-            }
-            System.out.printf("The random secret number is %s.", code);
-        } else {
+        while(!(size <= maxSize)) {
             System.out.printf("Error: can't generate a secret number with a length of %d " + 
-            "because there aren't enough unique digits.", size);
+            "because there aren't enough unique digits.\n", size);
+            System.out.println("Please, enter the secret code's length:");
+            size = scanner.nextInt();
+        }
+
+        String code = generate(size); 
+        int bulls = 0;
+        int cows = 0;
+        int turn = 1;
+        boolean run = true;
+        System.out.println("Okay, let's start a game!");
+        while(run) {
+            System.out.printf("Turn %d:", turn);
+            String input = scanner.next();
+            for (int i = 0; i < input.length(); i++) {
+                int index = code.indexOf(input.charAt(i));
+                if (index == i) {
+                    bulls++;
+                } else if (index >= 0) {
+                    cows++;
+                }
+            }
+            System.out.println(grade(bulls, cows, size));
+            if (bulls == size) {
+                break;
+            }
+            bulls = 0;
+            cows = 0;
+            turn++;
         }
         scanner.close();
+    }
+
+    public static String grade(int bulls, int cows, int size) {
+        String result;
+        if (bulls == size) {
+            result = String.format("Grade: %d bulls\nCongratulations! You guessed the secret code.", size);
+        } else if (bulls > 0 && cows > 0) {
+            result = String.format("%d bull(s) and %d cow(s)", bulls, cows);
+        } else if (bulls > 0) {
+            result = String.format("%d bull(s)", bulls);
+        } else if (cows > 0) {
+            result = String.format("%d cow(s)", cows);
+        } else {
+            result = "None";
+        }
+        return String.format("Grade: %s", result);
+    }
+
+    public static String generate(int size) {
+        Random random = new Random();
+        String code = "";
+        int maxDigit = 9;
+        int newDigit;
+        newDigit = random.nextInt(maxDigit) + 1;
+        code += newDigit;
+        while (code.length() < size) {
+            newDigit = random.nextInt(maxDigit);
+            if (code.contains(String.valueOf(newDigit))) {
+                continue;
+            }
+            code += newDigit;
+        }
+        return code;
     }
 }
